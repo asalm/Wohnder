be6f4fe1aab212b25d18006f79e6b784b3ed062a
Andre <asalmeri@stud.hs-bremen.de>
2020-06-23 13:30:24 +0200

    more features

--- app/tndr.js
+++ app/tndr.js
@@ -1,7 +1,63 @@
-// Source: https://codepen.io/diptinayan/pen/OqPWjR
+// Source: https://codepen.io/developingidea/pen/meAIn
+
 
 $(document).ready(function(){
+  var data = databasehandler();
+  addCards(data);
+  /*
+    <div class="buddy" style="display: block;">
+      <div class="avatar"  style="display: block; background-image: url(https://1.bp.blogspot.com/_qEbjiFbQWGM/TCBVlN3mkYI/AAAAAAAADCM/7CjYqUHwbgY/s1600/workshop_modell_0126.jpg)">
+      </div>
+      <h1 class="title">Title</h1>
+      <p class="has-text-black">Short description is super important so the user knows whats up with the flat they are trying to check out!</p>
+      <ol class="info">
+        <li>320€</li>
+        <li>17qm²</li>
+        <li>3 MB</li>
+      </ol>
+    <div>
+  */
 
+  function addCards(data){
+
+    var backgroundimg = "https://1.bp.blogspot.com/_qEbjiFbQWGM/TCBVlN3mkYI/AAAAAAAADCM/7CjYqUHwbgY/s1600/workshop_modell_0126.jpg"
+    var outerBody = document.createElement('div');
+    var avatar = document.createElement('div');
+    outerBody.classList.add('buddy');
+    outerBody.style = "display:block";
+    avatar.classList.add('avatar');
+    avatarBody.style = "display:block; background-image: url(" + backgroundimg + ")"; 
+    outerBody.appendChild(avatar);
+
+
+    document.getElementById('card-container').appendChild(outerBody);
+  }
+
+  function databasehandler(){
+    console.log("test");
+    $.get("http://localhost:8081/getWohnung", function(data, status){
+      //alert("Data: " + data + "\nStatus: " + status);
+      return data;
+    });
+    /*
+    $.ajax({
+      type: 'GET',
+      url: 'localhost:8081/getWohnung',
+      dataType: 'json',
+      success: function( data){
+        console.log(data);
+
+      }
+    });*/
+  }
+
+  //Menu Activation Handler
+    $('ol li').click(function(){
+      $('li').removeClass("active");
+      $(this).addClass("active");
+  });
+
+  //Input Swiperight Handler
     $(".buddy").on("swiperight",function(){
       $(this).addClass('rotate-left').delay(700).fadeOut(1);
       $('.buddy').find('.status').remove();
@@ -14,6 +70,7 @@ $(document).ready(function(){
        }
     });  
 
+  // Input Swipeleft  Handler
    $(".buddy").on("swipeleft",function(){
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
